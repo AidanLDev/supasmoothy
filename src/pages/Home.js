2dda566e249c55e2330238b0f57516c7a8f5d8e7
Aidan <dev@aidanlowson.com>
2023-07-06 23:32:43 +0100

    Finishing up the app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,24 @@
-import supabase from '../lib/supabaseClient';
-import { useEffect, useState } from 'react';
-import SmoothieCard from '../components/SmoothieCard';
+import supabase from "../lib/supabaseClient";
+import { useEffect, useState } from "react";
+import SmoothieCard from "../components/SmoothieCard";
 
 const Home = () => {
   const [fetchError, setFetchError] = useState(null);
   const [smoothies, setSmoothies] = useState(null);
+  const [orderBy, setOrderBy] = useState("created_at");
+
+  const handleDelete = (id) => {
+    setSmoothies((prevSmoothies) => {
+      return prevSmoothies.filter((smoothie) => smoothie.id !== id);
+    });
+  };
 
   useEffect(() => {
     const fetchSmoothies = async () => {
-      const { data, error } = await supabase.from('smoothies').select();
+      const { data, error } = await supabase
+        .from("smoothies")
+        .select()
+        .order(orderBy, { ascending: false });
 
       if (error) {
         setFetchError(`Fetching smoothie failed! ${error.message}`);
@@ -22,21 +32,32 @@ const Home = () => {
     };
 
     fetchSmoothies();
-  }, []);
+  }, [orderBy]);
 
   return (
-    <div className='page home'>
+    <div className="page home">
       {fetchError && (
         <div>
           <p>{fetchError}</p>
         </div>
       )}
       {smoothies && (
-        <div className='smoothies'>
-          {/* order-by buttons */}
-          <div className='smoothie-grid'>
+        <div className="smoothies">
+          <div className="order-by">
+            <p>Order By</p>
+            <button onClick={() => setOrderBy("created_at")}>
+              Created Date
+            </button>
+            <button onClick={() => setOrderBy("title")}>Title</button>
+            <button onClick={() => setOrderBy("rating")}>Rating</button>
+          </div>
+          <div className="smoothie-grid">
             {smoothies.map((smoothie, i) => (
-              <SmoothieCard key={`${smoothie.id}__${i}`} smoothie={smoothie} />
+              <SmoothieCard
+                key={`${smoothie.id}__${i}`}
+                smoothie={smoothie}
+                onDelete={handleDelete}
+              />
             ))}
           </div>
         </div>
