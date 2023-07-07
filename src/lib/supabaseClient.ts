7157573ad26ab690219fc811885fd43f9aa746a8
Aidan <aidan.lowson@davies-group.com>
2023-07-07 17:44:38 +0100

    Adding vite

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl: string = process.env.REACT_APP_SUPABASE_URL!;
-const supabaseKey: string = process.env.REACT_APP_ANON_KEY!;
+
+
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL!; //  process.env.REACT_APP_SUPABASE_URL!;
+const supabaseKey: string = import.meta.env.VITE_ANON_KEY!; //  process.env.REACT_APP_ANON_KEY!;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
